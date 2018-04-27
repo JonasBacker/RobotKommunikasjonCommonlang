85e8dec370c833661998c0be0c8ebaa2b825555d
JonasBacker <jonas_15_10_92@hotmail.com>
2018-04-27 09:41:23 +0200

    First commit
    First commit

---
+++ RobotComms/src/Arduino/R_dtLysArduino.ino
@@ -0,0 +1,212 @@
+#include <Servo.h>
+#include <SPI.h>
+#include <Pixy.h>
+#include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+
+Pixy pixy;
+Servo servoRight;
+Servo servoLeft;
+WiFiClient espClient;
+PubSubClient client(espClient);
+int touchingLeft;
+int touchingRight;
+int seeingLeft;
+int seeingRight;
+bool objectDetected;
+bool started = false; // Mqtt command on topic
+const char* ssid = "MagnusSugeIMarioCart";
+const char* password = "atiganger";
+const char* mqtt_server = "192.168.1.4";
+const char* tilEV3 = "tilEV3";
+const char* go = "go";
+const char* stopp = "stop";
+const char* loss = "loss";
+int x = 0;
+int width = 0; // ev3 block width
+int widthTemp = 0;
+
+
+void turnRight(int degrees) {
+  servoLeft.writeMicroseconds(1547);
+  servoRight.writeMicroseconds(1547);
+  delay(degrees * 11);
+  servoLeft.writeMicroseconds(1500);
+  servoRight.writeMicroseconds(1495);
+  delay(5);
+}
+
+void turnLeft(int degrees) {
+  servoLeft.writeMicroseconds(1453);
+  servoRight.writeMicroseconds(1453);
+  delay(degrees * 11);
+  servoLeft.writeMicroseconds(1500);
+  servoRight.writeMicroseconds(1495);
+  delay(5);
+}
+
+
+
+void turnTo() {
+
+  if (x < 100) {
+    turnLeft(-(x - 145) / 3);
+  }
+  else if (x > 180) {
+    turnRight((x - 145) / 3);
+  }
+
+}
+
+void getBlockData() {
+  int j;
+  uint16_t blocks;
+  char buf[32];
+
+  blocks = pixy.getBlocks();
+  blocks = pixy.getBlocks();
+  blocks = pixy.getBlocks();
+  blocks = pixy.getBlocks();
+  blocks = pixy.getBlocks();
+
+
+
+  sprintf(buf, "Detected %d:\n", blocks);
+  Serial.print(buf);
+
+  for (j = 0; j < blocks; j++)
+  {
+    sprintf(buf, "  block %d: ", j);
+    Serial.print(buf);
+    pixy.blocks[j].print();
+  }
+
+  x = pixy.blocks[0].x;
+  width = pixy.blocks[0].width;
+
+  Serial.print("x: ");
+  Serial.println(x);
+  Serial.print("width: ");
+  Serial.println(width);
+
+
+  if (!blocks) {
+    x = 0;
+  }
+
+}
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266 Client")) {
+      Serial.println("connected");
+      // ... and subscribe to topic
+      client.subscribe("arduino");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  for (int i = 0; i < length; i++) {
+    char receivedChar = (char)payload[i];
+    Serial.print(receivedChar);
+    if (receivedChar == '1') {
+      started = true;
+    }
+    if (receivedChar == '0') {
+      started = false;
+    }
+
+  }
+
+}
+
+void setup() {
+  servoRight.attach(D2); //Right Servo
+  servoLeft.attach(D3); //Left Servo
+  pinMode(D1, INPUT); // Left Whisker
+  pinMode(D0, INPUT); // Right Whisker
+  pinMode(D8, OUTPUT); //ledpin
+  client.setServer(mqtt_server, 1883); //Set client server
+  client.setCallback(callback); //Set callback method
+  servoRight.writeMicroseconds(1495);
+  Serial.begin(9600);
+  pixy.init();
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  if (started) {
+
+    client.loop();
+    if (started) {
+      digitalWrite(D8, LOW);
+      turnRight(190);
+    }
+    client.loop();
+    client.publish(tilEV3, go);
+    client.loop();
+    delay(3000);
+    client.loop();
+    if (started) {
+
+      // snu seg mot ev3
+      turnRight(100);
+      digitalWrite(D8, HIGH);
+      client.publish(tilEV3, stopp);
+      turnRight(100);
+
+      // finn EV3
+      while ((x == 0 || x < 100 || x > 180) && started) {
+        client.loop();
+        delay(500);
+        getBlockData();
+        delay(500);
+        turnTo();
+        Serial.println(x);
+      }
+
+      // sjekk for bevegelse
+      getBlockData();      // Hent width
+      widthTemp = width;   // Lage width i en temp variabel
+      delay(2000);
+      getBlockData();      // Hent ny width
+      if (width - widthTemp > 20) {
+        client.publish(tilEV3, loss);
+      }
+
+      // Reset x
+      x = 0;
+
+    }
+
+    client.loop();
+
+  }
+}
+
+
+
+
+
---
+++ RobotComms/src/ev3dev/Lys.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+
+import ev3dev.ev3 as ev3
+from time import sleep as _sleep
+import paho.mqtt.client as mqtt
+
+previousSonicRead = 0
+previousSonicRead2 = 0
+stopped = False
+sonicDiff = 0
+count = 0
+
+def setup():
+    global rightMotor
+    global leftMotor
+    global touchingLeft
+    global touchingRight
+    global seeingRight
+    global seeingLeft
+    global client
+    rightMotor = ev3.LargeMotor('outA')
+    leftMotor = ev3.LargeMotor('outB')
+    rightMotor.reset()
+    leftMotor.reset()
+    touchingLeft = False
+    touchingRight = False
+    seeingRight = 0
+    seeingLeft = 0
+    
+    client = mqtt.Client()
+    client.connect("192.168.1.4",1883,60)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    
+    
+def loop():
+    global previousSonicRead
+    global previousSonicRead2
+    global stopped
+    global sonicDiff
+    global count
+    while True:
+        client.loop()
+        ReadSensors()
+        previousSonicRead2 = previousSonicRead
+        previousSonicRead = ((seeingLeft+seeingRight)/2)
+        _sleep(0.5)
+        client.publish("tilPc", (seeingLeft+seeingRight/2))
+        if stopped:
+            count+=1
+        if stopped and count > 3:
+            if (previousSonicRead - previousSonicRead2) < 0:
+                sonicDiff = -(previousSonicRead - previousSonicRead2)
+            else: sonicDiff = (previousSonicRead - previousSonicRead2)
+            if (sonicDiff > 10):
+                moveForward(2)
+                stopped = false
+                count = 0
+        if Touching():
+            stop()
+            client.publish("arduino", "0")
+
+def main():
+    setup()
+    loop()
+
+
+#METODER
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code "+str(rc))
+  client.subscribe("tilEV3")
+
+def on_message(client, userdata, msg):
+  if msg.payload.decode() == "go":
+    moveForward(2)
+  if msg.payload.decode() == "stop":
+    stop()
+    stopped = True
+  if msg.payload.decode() == "loss":
+    turnLeft(180)
+    moveForward(2)
+    _sleep((2000-(seeingLeft+seeingRight/2))*0.01)
+    stop()
+    turnLeft(160)
+  client.publish("tilPc", "Message Confirm")
+
+def moveForward(time):
+    global rightMotor
+    global leftMotor
+    rightMotor.run_forever(speed_sp=100)
+    leftMotor.run_forever(speed_sp=100)
+    _sleep(time*0.001)
+def moveBackward(time):
+    global rightMotor
+    global leftMotor
+    rightMotor.run_direct(duty_cycle_sp=-75)
+    leftMotor.run_direct(duty_cycle_sp=-75)
+    _sleep(time*0.001)
+def turnLeft(degrees):
+    global rightMotor
+    global leftMotor
+    rightMotor.run_direct(duty_cycle_sp=37)
+    leftMotor.run_direct(duty_cycle_sp=-37)
+    initial = ev3.GyroSensor().value()
+    while ev3.GyroSensor().value() < initial + degrees :
+        _sleep(0.001)
+    rightMotor.reset()
+    leftMotor.reset()
+def turnRight(degrees):
+    global rightMotor
+    global leftMotor
+    rightMotor.run_direct(duty_cycle_sp=-37)
+    leftMotor.run_direct(duty_cycle_sp=37)
+    initial = ev3.GyroSensor().value()
+    while ev3.GyroSensor().value() > initial - degrees :
+        _sleep(0.001)
+    rightMotor.reset()
+    leftMotor.reset()
+def idle(time):
+    leftMotor.reset()
+    rightMotor.reset()
+    _sleep(time*0.001)
+def stop():
+    leftMotor.reset()
+    rightMotor.reset()
+def ReadSensors():
+    global touchingLeft
+    global touchingRight
+    global seeingLeft
+    global seeingRight
+    touchingLeft = ev3.TouchSensor().value()
+    touchingRight = ev3.TouchSensor().value()
+    seeingLeft = ev3.UltrasonicSensor().value()
+    seeingRight = ev3.UltrasonicSensor().value()
+def Touching():
+    global touchingLeft
+    global touchingRight
+    return touchingLeft or touchingRight
+def TouchingBoth():
+    global touchingLeft
+    global touchingRight
+    return touchingLeft and touchingRight
+def TouchingLeft():
+    global touchingLeft
+    return touchingLeft
+def TouchingRight():
+    global touchingRight
+    return touchingRight
+def Seeing():
+    global seeingLeft
+    global seeingRight
+    return seeingLeft or seeingRight
+def SeeingBoth():
+    global seeingLeft
+    global seeingRight
+    return seeingLeft  and  seeingRight
+def SeeingLeft():
+    global seeingLeft
+    return seeingLeft
+def SeeingRight():
+    global seeingRight
+    return seeingRight
+def MoveForward(time):
+    moveForward(time)
+def MoveBackward(time):
+    moveBackward(time)
+def TurnRight(degrees):
+    turnRight(degrees)
+def TurnLeft(degrees):
+    turnLeft(degrees)
+def Idle(time):
+    idle(time)
+main()
